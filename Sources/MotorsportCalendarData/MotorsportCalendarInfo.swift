92ef338e64d65d3c167e1d2a708a13f5a5d8b229
Łukasz Rutkowski <lukus178@gmail.com>
2024-09-11 12:58:21 +0200

    Handle unsupported series when decoding info

--- Sources/MotorsportCalendarData/MotorsportCalendarInfo.swift
+++ Sources/MotorsportCalendarData/MotorsportCalendarInfo.swift
@@ -13,4 +13,14 @@ public struct MotorsportCalendarInfo: Codable {
     package init(updates: [Series: Date]) {
         self.updates = updates
     }
+
+    public init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let rawUpdates = try container.decode([String: Date].self, forKey: .updates)
+        let seriesUpdates = rawUpdates.compactMap { (key, date) -> (Series, Date)? in
+            guard let series = Series(rawValue: key) else { return nil }
+            return (series, date)
+        }
+        self.updates = Dictionary(uniqueKeysWithValues: seriesUpdates)
+    }
 }
