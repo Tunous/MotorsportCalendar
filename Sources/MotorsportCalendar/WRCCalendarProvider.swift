c63aa203381a8e8ac644d69d92f913efd86f5f14
Łukasz Rutkowski <lukus178@gmail.com>
2025-10-10 11:54:44 +0200

    Minor WRC parser code cleanup

--- Sources/MotorsportCalendar/WRCCalendarProvider.swift
+++ Sources/MotorsportCalendar/WRCCalendarProvider.swift
@@ -18,12 +18,10 @@ struct WRCCalendarProvider: CalendarProvider {
     }
 
     func events(year: Int) async throws -> [MotorsportEvent] {
-        var calendar = Calendar.current
-        calendar.timeZone = .gmt
         let dateParser = Date.VerbatimFormatStyle(
             format: "\(day: .defaultDigits). \(month: .defaultDigits). \(year: .defaultDigits)",
             timeZone: .gmt,
-            calendar: calendar
+            calendar: .gmt
         ).parseStrategy
 
         let url = URL(string: "https://www.ewrc-results.com/season/\(year)/1-wrc/")!
@@ -60,24 +58,7 @@ struct WRCCalendarProvider: CalendarProvider {
                     stageStartDate = try dateParser.parse("\(stageDateText) \(year)")
                 }
 
-                // 12:00 21. 11. UTC
-                // 12:00 UTC
-                // 12:00 21. 11.
-                // 12:00
-                func parseUTCDate() throws -> Date? {
-                    let stageTimeNode = try stageNode.select(".harm-time").first()
-                    let stageTimeText = try stageTimeNode?.nextElementSibling()?.text() ?? stageTimeNode?.text() ?? ""
-                    guard let stageTimeMatch = stageTimeText.firstMatch(of: #/(?<hour>\d+):(?<minute>\d+)\s*((?<day>\d+)\. (?<month>\d+)\.)?\s*(UTC)?/#) else {
-                        return nil
-                    }
-                    let hour = Int(stageTimeMatch.output.hour)!
-                    let minute = Int(stageTimeMatch.output.minute)!
-                    let day = stageTimeMatch.output.day.map { Int($0)! } ?? calendar.component(.day, from: stageStartDate)
-                    let month = stageTimeMatch.output.month.map { Int($0)! } ?? calendar.component(.month, from: stageStartDate)
-                    return DateComponents(calendar: calendar, timeZone: .gmt, year: year, month: month, day: day, hour: hour, minute: minute, second: 0).date!
-                }
-
-                guard let date = try parseUTCDate() else {
+                guard let date = try parseStageUTCDate(from: stageNode, startDate: stageStartDate, year: year) else {
                     stages = []
                     break
                 }
@@ -95,23 +76,8 @@ struct WRCCalendarProvider: CalendarProvider {
                 )
             }
 
-            let maxDurationInHours = 3
-            for index in stages.indices.dropLast() {
-                stages[index + 1].startDate = max(stages[index].startDate, stages[index + 1].startDate)
+            tweakDates(of: &stages)
 
-                let nextStage = stages[index + 1]
-                let dateBeforeStartOfNextStage = max(stages[index].startDate.addingTimeInterval(10*60), nextStage.startDate.addingTimeInterval(-1))
-                let areStagesInSameDay = calendar.isDate(stages[index].startDate, inSameDayAs: nextStage.startDate)
-
-                if areStagesInSameDay && nextStage.startDate > stages[index].startDate && DateInterval(start: stages[index].startDate, end: nextStage.startDate).duration < (60*60 * TimeInterval(maxDurationInHours)) {
-                    stages[index].endDate = dateBeforeStartOfNextStage
-                } else {
-                    stages[index].endDate = min(calendar.date(byAdding: .hour, value: maxDurationInHours, to: stages[index].startDate)!, dateBeforeStartOfNextStage)
-                }
-            }
-            if !stages.isEmpty {
-                stages[stages.count - 1].endDate = calendar.date(byAdding: .hour, value: maxDurationInHours, to: stages[stages.count - 1].startDate)!
-            }
             let event = MotorsportEvent(
                 title: name.replacingOccurrences(of: year.description, with: "").trimmingCharacters(in: .whitespaces),
                 startDate: stages.first?.startDate ?? startDate,
@@ -153,5 +119,57 @@ struct WRCCalendarProvider: CalendarProvider {
             .domain: "www.ewrc-results.com",
         ])!
         HTTPCookieStorage.shared.setCookies([timezoneCookie], for: url, mainDocumentURL: nil)
+    }
+
+    // 12:00 21. 11. UTC
+    // 12:00 UTC
+    // 12:00 21. 11.
+    // 12:00
+    private func parseStageUTCDate(from stageNode: Element, startDate: Date, year: Int) throws -> Date? {
+        let stageTimeNode = try stageNode.select(".harm-time").first()
+        let stageTimeText = try stageTimeNode?.nextElementSibling()?.text() ?? stageTimeNode?.text() ?? ""
+        guard let stageTimeMatch = stageTimeText.firstMatch(of: #/(?<hour>\d+):(?<minute>\d+)\s*((?<day>\d+)\. (?<month>\d+)\.)?\s*(UTC)?/#) else {
+            return nil
+        }
+        let calendar = Calendar.gmt
+        let hour = Int(stageTimeMatch.output.hour)!
+        let minute = Int(stageTimeMatch.output.minute)!
+        let day = stageTimeMatch.output.day.map { Int($0)! } ?? calendar.component(.day, from: startDate)
+        let month = stageTimeMatch.output.month.map { Int($0)! } ?? calendar.component(.month, from: startDate)
+        return DateComponents(calendar: calendar, timeZone: .gmt, year: year, month: month, day: day, hour: hour, minute: minute, second: 0).date!
+    }
+
+    private func tweakDates(of stages: inout [MotorsportEventStage]) {
+        let maxDurationInHours = 3
+        let calendar = Calendar.gmt
+        for index in stages.indices.dropLast() {
+            // Stages sometimes are marked as starting earlier than previous.
+            // Override the start date for them to start date of previous to always have dates in order.
+            stages[index + 1].startDate = max(stages[index].startDate, stages[index + 1].startDate)
+
+            let nextStage = stages[index + 1]
+            let dateBeforeStartOfNextStage = max(stages[index].startDate.addingTimeInterval(10*60), nextStage.startDate.addingTimeInterval(-1))
+            let areStagesInSameDay = calendar.isDate(stages[index].startDate, inSameDayAs: nextStage.startDate)
+
+            if areStagesInSameDay && nextStage.startDate > stages[index].startDate && DateInterval(start: stages[index].startDate, end: nextStage.startDate).duration < (60*60 * TimeInterval(maxDurationInHours)) {
+                // For stages that are in the same day and are close to each other order set their end dates to be connected
+                stages[index].endDate = dateBeforeStartOfNextStage
+            } else {
+                // Otherwise set end date up to the specified hours limit
+                stages[index].endDate = min(calendar.date(byAdding: .hour, value: maxDurationInHours, to: stages[index].startDate)!, dateBeforeStartOfNextStage)
+            }
+        }
+        if !stages.isEmpty {
+            // Set end time for last stage of event to max value
+            stages[stages.count - 1].endDate = calendar.date(byAdding: .hour, value: maxDurationInHours, to: stages[stages.count - 1].startDate)!
+        }
+    }
+}
+
+extension Calendar {
+    static var gmt: Calendar {
+        var calendar = Calendar.current
+        calendar.timeZone = .gmt
+        return calendar
     }
 }
