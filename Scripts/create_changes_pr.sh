82634636f1c013c57f26325c34d8ad8023fb5238
Łukasz Rutkowski <lukus178@gmail.com>
2026-06-25 19:37:15 +0200

    Mention me in PR, edit existing PR if exists

--- Scripts/create_changes_pr.sh
+++ Scripts/create_changes_pr.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -euo pipefail
+
 printf "# Changes\n\n" > /tmp/changes.md
 for file in $(git --no-pager diff HEAD~1 HEAD --name-only); do
   printf "<details><summary>%s</summary>\n\n\`\`\`diff\n" "$file" >> /tmp/changes.md
@@ -7,4 +9,10 @@ for file in $(git --no-pager diff HEAD~1 HEAD --name-only); do
   printf "\n\`\`\`\n</details>\n\n" >> /tmp/changes.md
 done
 
-gh pr create --title "Update schedule" --body-file /tmp/changes.md
+printf "@Tunous\n" >> /tmp/changes.md
+
+if pr_number="$(gh pr view --json number --jq .number 2>/dev/null)"; then
+  gh pr edit "$pr_number" --title "Update schedule" --body-file /tmp/changes.md
+else
+  gh pr create --title "Update schedule" --body-file /tmp/changes.md
+fi
