2d2cd46015285d04a163af39a1455cbd16266ced
Łukasz Rutkowski <lukus178@gmail.com>
2026-07-15 10:28:11 +0200

    Revert "Revert script change that decided to edit or create pr"
    This reverts commit 6e095a6982e1ef6c5ce65708001d7824623c48bd.

--- Scripts/create_changes_pr.sh
+++ Scripts/create_changes_pr.sh
@@ -9,4 +9,8 @@ done
 
 printf "@Tunous\n" >> /tmp/changes.md
 
-gh pr create --title "Update schedule" --body-file /tmp/changes.md
+if pr_number="$(gh pr view --json number --jq .number 2>/dev/null)"; then
+  gh pr edit "$pr_number" --title "Update schedule" --body-file /tmp/changes.md
+else
+  gh pr create --title "Update schedule" --body-file /tmp/changes.md
+fi
